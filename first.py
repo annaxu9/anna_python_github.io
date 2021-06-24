5c1a3f30a5204bd245684eb7d5f07063b43b86c9
Anna Xu <77125837+annaxu9@users.noreply.github.com>
2021-06-24 16:53:34 -0400

    Add files via upload

--- first.py
+++ first.py
@@ -1,7 +1,21 @@
+import math
+
+# 1. is_even assignment
+# 2. solve_quadratic assignment
+
 def is_even(x):
     if (x % 2) == 1:
         return False
     else:
         return True
 
-is_even(99)
+def solve_quadratic(a,b,c):
+    if math.pow(b,2) >= (4 * a * c):
+        x = (-b + math.sqrt(math.pow(b,2) - 4 * a * c))/2
+        y = (-b - math.sqrt(math.pow(b,2) - 4 * a * c))/2
+        if (x != y):
+            return (x,y)
+        else:
+            return x
+    else:
+        return None
